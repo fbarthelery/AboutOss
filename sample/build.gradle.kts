97f113c5e07c55a3bd1393c29bbbe6a3297958c5
Da Risk <da_risk@geekorum.com>
2025-09-16 15:01:49 -0400

    build: update to agp 8.13.0

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -35,7 +35,7 @@ plugins {
 // workaround bug https://issuetracker.google.com/issues/275534543
 buildscript {
     dependencies {
-        classpath("com.android.tools.build:gradle:8.9.2")
+        classpath("com.android.tools.build:gradle:8.13.0")
     }
 }
 
